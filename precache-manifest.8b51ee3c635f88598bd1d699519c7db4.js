73fc470a7ebe5fd3ce3adc6c1d8624b1ca108806
ggank <13955056+kaygrog@users.noreply.github.com>
2020-07-20 21:53:22 -0500

    Updates

---
+++ precache-manifest.8b51ee3c635f88598bd1d699519c7db4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8738f05716eae0a6d6dc4ccfc7a104dd",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c729cae43f004fba7768",
+    "url": "/Clicky-Game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03c5981147b2306e3af8",
+    "url": "/Clicky-Game/static/js/2.030fdbd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.030fdbd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c729cae43f004fba7768",
+    "url": "/Clicky-Game/static/js/main.aba6878e.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
